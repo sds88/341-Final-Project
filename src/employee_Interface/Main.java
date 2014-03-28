31adc54af29591a8008f2694d289ac6504086936
sds88 <sds88@case.edu>
2014-03-27 22:40:46 -0400

    Product search

--- src/employee_Interface/Main.java
+++ src/employee_Interface/Main.java
@@ -2,6 +2,7 @@ package employee_Interface;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Main {
 	public static MySQLConnection conn;
@@ -141,13 +142,13 @@ public class Main {
 			sql += "Customers.`Phone Number` = "+para+";";
 			conn.ExecuteQuery(sql);
 			break;
-		case '4':
+		case '4': // Do not need to check if the entered string is valid
 			sql += "Customers.Address LIKE \"%"+para+"%\";";
 			conn.ExecuteQuery(sql);
+			break;
 		default:
 			break;
 		}
-		
 	}
 	
 	private static Boolean IsValidCustomerID(String id)
@@ -172,7 +173,104 @@ public class Main {
 	
 	private static void EnterProductState()
 	{
-		
+		ClearConsole();
+		System.out.println("Please select which parameter to search with:");
+		System.out.println("1. UPC Code (XXXXXXXXXX).");
+		System.out.println("2. Name.");
+		System.out.println("3. Brand.");
+		System.out.println("4. MSRP (XX.XX).");
+		System.out.println("5. Category.");
+		System.out.println("6. Back.");
+		char selection = ReadChar();
+		if(selection == '6')
+		{
+			state = State.HOME;
+			return;
+		} 
+		else if(selection < '1' || selection > '6')
+		{
+			System.out.println("Invalid selection.");
+			return;
+		}
+		System.out.println("Enter the search string:");
+		String para = ReadLine();
+		String sql = "Select * FROM Products Where ";
+		switch(selection) {
+		case '1':
+			while(!IsValidUPC(para))
+			{
+				System.out.println("Invalid UPC code.");
+				System.out.println("Please enter a UPC code in the form of a group of 12 digits (i.e. 123412341234):");
+				para = ReadLine();
+			}
+			sql += "Products.`UPC Code` = "+para+";";
+			conn.ExecuteQuery(sql);
+			break;
+		case '2':	// Do not need to check if the entered string is valid
+			sql += "Products.`Product Name` LIKE \"%"+para+"%\";";
+			conn.ExecuteQuery(sql);
+			break;
+		case '3':	// Do not need to check if the entered string is valid
+			sql += "Products.Brand LIKE \"%"+para+"\"%;";
+			conn.ExecuteQuery(sql);
+			break;
+		case '4': // TODO: Maybe add the option to search for greater/less than a number
+			while(!IsValidMSRP(para))
+			{
+				System.out.println("Invalid MSRP amount.");
+				System.out.println("Please enter an MSRP amount in the form of 'dollars.'cents' (i.e. 10.99):");
+				para = ReadLine();
+			}
+			sql += "Products.`MSRP` = "+para+";";
+			conn.ExecuteQuery(sql);
+			break;
+		case '5': // Do not need to check if the entered string is valid
+			sql += "Product.Categories LIKE \"%"+para+"%\";";
+			conn.ExecuteQuery(sql);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private static Boolean IsValidUPC(String upc)
+	{
+		// Check length
+		if(upc.length() != 12)
+			return false;
+		// Check all characters are digits 0-9
+		for(char c : upc.toCharArray())
+		{
+			if(c < '0' || c > '9')
+				return false;
+		}
+		return true;
+	}
+	
+	private static Boolean IsValidMSRP(String msrp)
+	{
+		/* Lol.  Taken from double.valueOf() entry in the java api */
+		final String Digits     = "(\\p{Digit}+)";
+		final String HexDigits  = "(\\p{XDigit}+)";
+		// an exponent is 'e' or 'E' followed by an optionally signed decimal integer.
+		final String Exp        = "[eE][+-]?"+Digits;
+		final String fpRegex    = 
+				("[\\x00-\\x20]*"+	// Optional leading "whitespace"
+				"[+-]?(" +			// Optional sign character
+				"NaN|" +          	// "NaN" string
+				"Infinity|" +      	// "Infinity" string		             	
+		        "((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|" +	// Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt		          
+		        "(\\.("+Digits+")("+Exp+")?)|"+	// . Digits ExponentPart_opt FloatTypeSuffix_opt
+		        "((" +		        
+		        "(0[xX]" + HexDigits + "(\\.)?)|" + // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt		        
+		       	"(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" + // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+		       	")[pP][+-]?" + Digits + "))" +
+		       	"[fFdD]?))" +
+				"[\\x00-\\x20]*");// Optional trailing "whitespace"
+		            
+		  if (Pattern.matches(fpRegex, msrp))
+		            return true;
+		  return false;
 	}
 	
 	private static void EnterInventoryState()
